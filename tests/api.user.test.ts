52f292719fc1b8e5151b2cdbd3df3ecbe1647eb5
luluopa <luluopa@hotmail.com>
2022-07-25 21:37:02 -0300

    test: creating the rest of api's test

--- tests/api.user.test.ts
+++ tests/api.user.test.ts
@@ -1,22 +1,23 @@
 import request from 'supertest'
 import app from '../src/index'
-import { USER_CREATE_URL, USER_DEFAULT_URL } from '../src/util/constants/api_routers/user.api'
+import { LIST_USER_URL, USER_CREATE_URL, USER_DEFAULT_URL, USER_DELETE_URL, USER_UPDATE_URL } from '../src/util/constants/api_routers/user.api'
 
 //criar teste para conexao com o banco ok
-//criar testes unitarios para a api ...
-
-//testar o criar ok
-//testar o deletar
-//testar o update
-//testar o get para usuario unico e lista de usuarios
+//criar testes unitarios para a api ok 
 
 describe('GET /user' + USER_DEFAULT_URL, () => {
-    it('[Testing user get api] Should return 200 Ok', () => {
-        return request(app).get(USER_DEFAULT_URL).then((response) => {
+    it('[Testing user get api] Should return 200 OK', () => {
+        return request(app).get('/user' + USER_DEFAULT_URL).then((response) => {
             expect(response.statusCode).toBe(200)
         })
     })
 })
+
+describe('GET /user' + LIST_USER_URL, () => {
+    it('[Testig user list api] Should return 200 OK', () => {
+        return request(app).get('/user' + LIST_USER_URL).expect(200)
+    })
+}) 
 
 describe('POST /user' + USER_CREATE_URL, () => {
     it('[Testing user create api] Should return 201 OK', () => {
@@ -27,10 +28,20 @@ describe('POST /user' + USER_CREATE_URL, () => {
             email: 'teste@gmail.com'
         }
 
-        return request(app).post('/user/' + USER_CREATE_URL).send(user).set('Content-Type', 'application/json').set('Accept', 'application/json').then((response) => {
+        return request(app).post('/user' + USER_CREATE_URL).send(user).set('Content-Type', 'application/json').set('Accept', 'application/json').then((response) => {
             expect(response.statusCode).toBe(201)
         })
     })
 })
 
-//o teste precisa deleter o mesmo usuario que criou no banco
+describe('UPDATE /user' + USER_UPDATE_URL, () => {
+    it('[Testing user update api] Should return 200 OK', () => {
+        return request(app).put('/user' + USER_UPDATE_URL).expect(200)
+    })
+})
+
+describe('DELETE /user' + USER_DELETE_URL, () => {
+    it('[Testing user delete api] Should return 200 OK', () => {
+        return request(app).delete('/user' + USER_DELETE_URL).expect(200)
+    })
+})
